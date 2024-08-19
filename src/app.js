968ee3c7d2fcb65d11f0245ddfc0867eabb42a66
Alexader Quejada cordoba <quejadacordobaalexander@gmail.com>
2024-08-19 07:45:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extencion = [".com", ".net", ".us", ".io"];
+  let domainlist = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let F = 0; F < extencion.length; F++) {
+          let domain = pronoun[i] + adj[y] + noun[j] + extencion[F];
+          domainlist.push(domain);
+        }
+      }
+    }
+  }
+  const domain = document.getElementById("domain");
+  //domain.textContent = domainlist;
+
+  console.log(domainlist);
+  domainlist.forEach(domainName => {
+    const li = document.createElement("li");
+    li.textContent = domainName;
+    domain.appendChild(li);
+  });
+  return domainlist;
 };
